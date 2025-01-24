97e84e4513ce890db93d07aa13cf612a0fee8d2b
fabio <fabioumpierre97@gmail.com>
2025-01-23 22:15:18 -0300

    add run.py and init.py for project setup

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
+import os
+from dotenv import load_dotenv
 
 
+load_dotenv()
+
+class Config:
+   
+    CLIENT_SECRETS_FILE = os.getenv("CLIENT_SECRETS_FILE")
+    SCOPES = os.getenv("SCOPES").split(",") 
+    API_SERVICE_NAME = os.getenv("API_SERVICE_NAME")
+    API_VERSION = os.getenv("API_VERSION")
+    REDIRECT_URI = os.getenv("REDIRECT_URI")
